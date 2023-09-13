e0afd62b389f53c845d9ccee4d4fbc3dd85e18f1
liam.soderstrom@bahnhof.se <97706771+MeetLos@users.noreply.github.com>
2023-09-13 09:06:18 +0200

    Update primtal_modul.py
    Kom på lössningen medans jag försökte sova.

--- primtal_modul.py
+++ primtal_modul.py
@@ -4,14 +4,19 @@ number_list = [*range(2,100)]
 # Lista som håller primtalen
 prime_list = []
 
-
+# Ittererar igenom numrerna.
 for number in number_list:
+    
+    # Delar numret med numrerna 2 till 99.
     for devider in range(2, number + 1):
         result = number / devider
+        
+        # Kollar om float värdet delas jämnt och om variablerna är densamma.
         if str(result).endswith("0"):
-            if (number / (devider - 1)) > (number - 1 / (devider - 2 or 1  )):
+            if number != devider:
+                break
+            else:
                 prime_list.append(number)
         
-            
-        
+# Printar resultatet.
 print(prime_list)
